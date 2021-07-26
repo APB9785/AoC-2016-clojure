cd0ebc1b627bfa7d55d4ab87231c9082f402bbe2
APB9785 <74077809+APB9785@users.noreply.github.com>
2021-07-26 18:01:09 -0500

    Day 9 Part 2

--- day_09.clj
+++ day_09.clj
@@ -3,18 +3,36 @@
 (def input
   (string/replace (slurp "day_9_input.txt") #"\s" ""))
 
-(defn run [todo]
-  (let [[cmd new-todo] (string/split (string/join (rest todo)) #"\)" 2)
-        [len reps] (string/split cmd #"x" 2)]
-    [(drop (Integer. len) new-todo) (* (Integer. len) (Integer. reps))]))
+(defn split-cmd [text]
+  (string/split (string/join (rest text)) #"\)" 2))
 
 (defn decompress-v1 [text]
   (loop [todo text
          cnt 0]
     (case (first todo)
           nil cnt
-          \( (let [[new-todo n] (run todo)] (recur new-todo (+ n cnt)))
-          (recur (rest todo) (+ 1 cnt)))))
+          \( (let [[cmd new-todo] (split-cmd todo)
+                   [len reps] (string/split cmd #"x" 2)]
+               (recur (drop (Integer. len) new-todo)
+                      (+ cnt (* (Integer. len) (Integer. reps)))))
+          (recur (rest todo)
+                 (+ 1 cnt)))))
+
+(defn decompress-v2 [text]
+  (loop [todo text
+         cnt 0]
+    (case (first todo)
+          nil cnt
+          \( (let [[cmd new-todo] (split-cmd todo)
+                   [len reps] (string/split cmd #"x" 2)
+                   [inner outer] (split-at (Integer. len) new-todo)]
+               (recur outer
+                      (+ cnt (* (decompress-v2 inner) (Integer. reps)))))
+          (recur (rest todo)
+                 (+ 1 cnt)))))
 
 ;; Part 1
 (decompress-v1 input)
+
+;; Part 2
+(decompress-v2 input)
